7ef9b61aad3984462de9744f0a4bbc98a6d141f1
TestGITEnv <iparichay@gmail.com>
2021-05-02 14:38:42 +0000

    Test pipeline with manual review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from TestEnv!'));
+app.get('/', (req, res) => res.send('Hello World from TestEnv!. Testing Pipleline with manual approval'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
